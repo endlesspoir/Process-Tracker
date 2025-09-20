a019c3de17572e5e8e0e3b5cad7eb5df51fa3a8e
Endlesspoir <art20055652@gmail.com>
2025-09-20 10:46:10 +0300

    Create models

--- src/main/java/org/example/Lab1/ProcessInfo.java
+++ src/main/java/org/example/Lab1/ProcessInfo.java
@@ -1,4 +1,17 @@
 package org.example.Lab1;
 
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class ProcessInfo {
+
+    private long pid;
+    private String user;
+    private String name;
+    private long memory;
+    private  int priority;
+    private int threads;
 }
--- src/main/java/org/example/Lab1/ThreadInfo.java
+++ src/main/java/org/example/Lab1/ThreadInfo.java
@@ -1,4 +1,11 @@
 package org.example.Lab1;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
 public class ThreadInfo {
+    private long tid;
+    private long priority;
 }
